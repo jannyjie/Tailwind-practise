77e5904f801bcb218ce4dd57c9aa4437a7954653
jannyyijie <yuaog1331@gmail.com>
2022-03-28 18:08:31 +0800

    axiosApi

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -3,9 +3,18 @@
 </template>
 
 <script>
-export default {
-
-}
+  import { onMounted } from "vue";
+  import { getName } from '../api/axios.js';
+  export default {
+    setup() {
+      onMounted(() => {
+        getName().then((res) => {
+          console.log(res);
+        });
+      });
+      return {}
+    },
+  }
 </script>
 
 <style>
